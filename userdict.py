435fa5a0a8d6629894dae220922d9428e0d198df
devsolve <naskararghya620@gmail.com>
2020-12-09 15:18:38 +0530

    Initial commit

---
+++ userdict.py
@@ -0,0 +1,12 @@
+print('Welcome to the Dictionary')
+
+d1 = {'programming': 'process of write program in computer', 'num': 'number',
+      'str': 'String', 'bool': 'boolean value'}
+
+print('The required meaning is:')
+
+search = input()
+
+print(d1[search])
+
+print('Thanks for using dictionary')
